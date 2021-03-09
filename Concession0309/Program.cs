d238126a5b280569527f691b3da0ce4db5a5a8f8
SID1EGR <SID1EGR@EGRZ0008>
2021-03-09 12:08:04 +0100

    Add project files.

---
+++ Concession0309/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession0309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
