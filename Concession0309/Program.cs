302edb9e82d0a1a874d4344d130b941f6242ca51
SID1EGR <SID1EGR@EGRZ0008>
2021-03-09 12:30:26 +0100

    commit2

--- Concession0309/Program.cs
+++ Concession0309/Program.cs
@@ -11,6 +11,7 @@ namespace Concession0309
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("HelloFrom Banch");
             Console.ReadLine();
         }
     }
